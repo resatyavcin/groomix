aa4d8237b8b024ac06fff988c429d7716e437aab
Reşat Yavçin <resatyavcin@icloud.com>
2025-11-02 16:28:59 +0300

    feat: add register admin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 
+import authRouter from "./routes/auth";
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,14 @@ const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_ANON_KEY!
 );
+
+app.use((req, res, next) => {
+  // @ts-ignore -> express Request tipine custom property ekliyoruz
+  req.supabase = supabase;
+  next();
+});
+
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Supabase + Express + TypeScript API 🚀");
---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,9 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      supabase: SupabaseClient;
+    }
+  }
+}
